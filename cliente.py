d1cff5531d8d893353047c448efe37fe13810a50
Mário Narcizo Júnior <129921507+MarioNarcizoJr@users.noreply.github.com>
2025-03-31 23:45:38 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,1 +1,12 @@
 
+import asyncio
+import websockets
+import json
+
+
+async def connect_to_server():
+    async with websockets.connect("ws://localhost:8765") as websocket:
+        while True:
+            question_data = await websocket.recv()
+            question = json.loads(question_data)
+            # Atualizar a interface do jogo com a nova pergunta
